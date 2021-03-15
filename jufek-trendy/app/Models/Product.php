cbe19fb86600df5a6d747a49bdfcd50253bdb83e
Juan Camilo <juancami98.11@hotmail.com>
2021-03-15 17:01:33 -0500

    model and migrate product

---
+++ jufek-trendy/app/Models/Product.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    //attributes id, name, size, stock, price, image, description, created_at, updated_at
+    protected $fillable = [
+        'name', 'size', 'stock', 'price', 'image', 'description',
+    ];
+
+    public static function validate(Request $request)
+    {
+        $request->validate([
+            "name" => "required",
+            "size" => "required",
+            "stock" => "required|numeric|gt:-1",
+            "price" => "required|numeric|gt:0",
+            "image" => "nullable",
+            "description" => "nullable",
+        ]);
+    }
+
+    public function getId()
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setId($id)
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getName()
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setName($name)
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getSize()
+    {
+        return $this->attributes['size'];
+    }
+
+    public function setSize($size)
+    {
+        $this->attributes['size'] = $size;
+    }
+
+    public function getPrice()
+    {
+        return $this->attributes['price'];
+    }
+
+    public function setPrice($price)
+    {
+        $this->attributes['price'] = $price;
+    }
+
+    public function getStock()
+    {
+        return $this->attributes['stock'];
+    }
+
+    public function setStock($stock)
+    {
+        $this->attributes['stock'] = $stock;
+    }
+
+    public function getImage()
+    {
+        return $this->attributes['image'];
+    }
+
+    public function setImage($image)
+    {
+        $this->attributes['image'] = $image;
+    }
+
+    public function getDescription()
+    {
+        return $this->attributes['description'];
+    }
+
+    public function setDescription($description)
+    {
+        $this->attributes['description'] = $description;
+    }
+}
---
+++ jufek-trendy/database/migrations/2021_02_20_000000_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+
+/**
+ * Run the migrations.
+ *
+ * @return void
+ */
+
+    public function up()
+    {
+
+        Schema::create('products', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->text('size');
+            $table->integer('stock');
+            $table->float('price');
+            $table->text('image')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+/**
+ * Reverse the migrations.
+ *
+ * @return void
+ */
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+
+}
