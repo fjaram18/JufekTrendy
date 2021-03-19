c31c8c2d3bf3f4de3778a7510365ed65ea21fa36
Federico Jaramillo <fjaram18@eafit.edu.co>
2021-03-19 13:53:30 -0500

    Customizations initial commit

--- jufek-trendy/routes/web.php
+++ jufek-trendy/routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name("ho
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/customization/show/{id}', [App\Http\Controllers\CustomizationController::class, 'show'])->name('customization.show');
+
+Route::get('/customization/create', [App\Http\Controllers\CustomizationController::class, 'create'])->name('customization.create');
+
+Route::get('/customization/save', [App\Http\Controllers\CustomizationController::class, 'save'])->name('customization.save');
+
+Route::get('/customization/list', [App\Http\Controllers\CustomizationController::class, 'list'])->name('customization.list');
+
+Route::get('/customization/delete/{id}', [App\Http\Controllers\CustomizationController::class, 'delete'])->name('customization.delete');
