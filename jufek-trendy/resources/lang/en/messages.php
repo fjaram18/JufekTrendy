37bec1175d78d2136780eb4d7a1aeaeb92165a5c
Juan Camilo <juancami98.11@hotmail.com>
2021-03-19 23:25:21 -0500

    añññadiendo lang

---
+++ jufek-trendy/resources/lang/en/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    //Layout messages
+    'login' => 'Login',
+    'register' => 'Register',
+    'logout' => 'Logout',
+    'location' => 'Location',
+    'around' => 'Around The Web',
+    'aboutJufek' => 'About Jufek Trendy',
+    'aboutJufekMessage' => 'Jufek Trendy is Comfort and Personality',
+
+];
