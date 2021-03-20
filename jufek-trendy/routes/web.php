95c5eff1622f49dd999e0962aceefeaf04466bb7
Juan Camilo <juancami98.11@hotmail.com>
2021-03-20 02:10:17 -0500

    modificando rutas, migraciones y modelos

--- jufek-trendy/routes/web.php
+++ jufek-trendy/routes/web.php
@@ -13,20 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name("home.index");
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home/admin', [App\Http\Controllers\Admin\AdminHomeController::class, 'index'])->name('admin.home');
 
+// Auth
 Auth::routes();
 
-Route::get('/customization/show/{id}', [App\Http\Controllers\CustomizationController::class, 'show'])->name('customization.show');
-Route::get('/customization/create', [App\Http\Controllers\CustomizationController::class, 'create'])->name('customization.create');
-Route::get('/customization/save', [App\Http\Controllers\CustomizationController::class, 'save'])->name('customization.save');
-Route::get('/customization/list', [App\Http\Controllers\CustomizationController::class, 'list'])->name('customization.list');
-Route::get('/customization/delete/{id}', [App\Http\Controllers\CustomizationController::class, 'delete'])->name('customization.delete');
+// Routes Admin Customization
+Route::get('/customization/show/{id}', [App\Http\Controllers\Admin\AdminCustomizationController::class, 'show'])->name('customization.show');
+Route::get('/customization/create', [App\Http\Controllers\Admin\AdminCustomizationController::class, 'create'])->name('customization.create');
+Route::get('/customization/save', [App\Http\Controllers\Admin\AdminCustomizationController::class, 'save'])->name('customization.save');
+Route::get('/customization/list', [App\Http\Controllers\Admin\AdminCustomizationController::class, 'list'])->name('customization.list');
+Route::get('/customization/delete/{id}', [App\Http\Controllers\Admin\AdminCustomizationController::class, 'delete'])->name('customization.delete');
 
-Route::get('/product/show/{id}', [App\Http\Controllers\ProductController::class, 'show'])->name("product.show");
-Route::get('/product/create', [App\Http\Controllers\ProductController::class, 'create'])->name("product.create");
-Route::post('/product/save', [App\Http\Controllers\ProductController::class, 'save'])->name("product.save");
-Route::get('/product/list', [App\Http\Controllers\ProductController::class, 'list'])->name("product.list");
-Route::get('/product/delete/{id}', [App\Http\Controllers\ProductController::class, 'delete'])->name("product.delete");
+// Routes Admin Product
+Route::get('/product/show/{id}', [App\Http\Controllers\Admin\AdminProductController::class, 'show'])->name("product.show");
+Route::get('/product/create', [App\Http\Controllers\Admin\AdminProductController::class, 'create'])->name("product.create");
+Route::post('/product/save', [App\Http\Controllers\Admin\AdminProductController::class, 'save'])->name("product.save");
+Route::get('/product/list', [App\Http\Controllers\Admin\AdminProductController::class, 'list'])->name("product.list");
+Route::get('/product/delete/{id}', [App\Http\Controllers\Admin\AdminProductController::class, 'delete'])->name("product.delete");
 
